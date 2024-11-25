b9a7b0b0ef64d77806d46216b561edc941d486ee
Rian Neris <86022706+RianNeris@users.noreply.github.com>
2024-11-25 16:22:41 -0300

    Should throw exception

---
+++ src/main/java/school/sptech/treino/service/CalculadoraService.java
@@ -0,0 +1,16 @@
+package school.sptech.treino.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+@Service
+public class CalculadoraService {
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Divisão por zero não permitida");
+        }
+        return a / b;
+    }
+}
